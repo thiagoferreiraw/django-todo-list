49c87df06016eb3e372390edce4614bcd2084b91
Thiago Ferreira <th.thiago96@gmail.com>
2019-10-19 18:49:18 -0300

    Added global pagination on settings

--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,8 @@ from rest_framework import generics, viewsets
 from .models import Task
 from .serializers import TaskSerializer
 
-generics.GenericAPIView
-
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    queryset = Task.objects.all().order_by("-id")
     serializer_class = TaskSerializer
+    max_limit = 50
