3b4018223bc8f0714ddc5ac45966358bfb74f1f9
Thiago Ferreira <th.thiago96@gmail.com>
2021-05-25 10:32:44 -0300

    feat: nested serializers on drf

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+
+
+class TaskItem(models.Model):
+    description = models.CharField(max_length=250)
+    status = models.BooleanField(default=False)
 
 
 class Task(models.Model):
     title = models.CharField(max_length=250)
     status = models.BooleanField(default=False)
+    task_items = models.ManyToManyField(TaskItem)
 
     def __str__(self):
         return self.title
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,35 @@
 from rest_framework import serializers
 
-from tasks.models import Task
+from tasks.models import Task, TaskItem
+
+
+class TaskItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskItem
+        fields = ("id", "description", "status")
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    task_items = TaskItemSerializer(many=True)
+
     class Meta:
         model = Task
         fields = "__all__"
+
+    def create(self, validated_data):
+        task_items = (
+            TaskItem.objects.create(**task_item)
+            for task_item in validated_data.pop("task_items")
+        )
+        task = Task.objects.create(**validated_data)
+        task.task_items.add(*task_items)
+
+        return task
+
+    def update(self, instance, validated_data):
+        for task_item in validated_data.pop("task_items"):
+            print(instance, task_item)
+            TaskItem.objects.filter(id=task_item.get("id")).update(**task_item)
+        Task.objects.filter(id=instance.id).update(**validated_data)
+        instance.refresh_from_db()
+        return instance
